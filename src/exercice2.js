0c7b7b3f593480b0cbdcb323f280d9491ab875fb
Sarah Tordeur <sarahmorgantordeur@gmail.com>
2025-11-17 14:07:06 +0100

    exo2

--- src/exercice2.js
+++ src/exercice2.js
@@ -7,7 +7,7 @@ const tuples = [
 ];
 // La fonction doit trier les tuples par âge croissant et retourner le tableau trié. 
 function trierParAge(personnes) {
-    let personnesTriees = personnes.sort((a, b) => a[1] - b[1]);
+    let personnesTriees = personnes.sort((a, b) => b[1] - a[1]);
     return personnesTriees;
 }
 console.log(trierParAge(tuples));
--- src/exercice2.ts
+++ src/exercice2.ts
@@ -15,7 +15,10 @@ const tuples: Array<Personnes> = [
 // La fonction doit trier les tuples par âge croissant et retourner le tableau trié. 
 
 function trierParAge(personnes: Array<Personnes>) : Array<Personnes> {
-    let personnesTriees : Personnes[] = personnes.sort((a, b) => a[1] - b[1]);
+    let personnesTriees : Personnes[] = personnes.sort(([, ageA], [, ageB]) => ageA - ageB); //déstructuration des tuples pour accéder directement aux âges, c'est typé donc il sait que la seconde position c'est un nombre
+    //si on veut une copie on utilise la méthode slice ou ça :
+    //[...personnes] => on fait une copie du tableau en le déstructurant
+    //ensuite on trie la copie et on la renvoie
     return personnesTriees;
 }
 
